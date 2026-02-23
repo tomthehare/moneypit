6b14247be1e33bed8751ec053b2498fb0e21518f
Tom Hare <thomasthehare@gmail.com>
2026-02-22 20:06:48 -0500

    Fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,8 @@ cd /home/thare/Apps/moneypit
 echo "Pulling latest code..."
 git pull origin main
 
-echo "Activating venv..."
-source venv/bin/activate
-
 echo "Installing dependencies..."
-pip install -r requirements.txt
+venv/bin/pip install -r requirements.txt
 
 echo "Restarting service..."
 sudo systemctl restart moneypit-app
